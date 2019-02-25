64d15f749a2443a63c4e88e9f8279aaf30802e75
Ahmed Raza Qadri <razatechuc11@gmail.com>
2019-02-25 19:37:20 +0500

    server is ready to go

--- api/config/db.js
+++ api/config/db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 
 module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'taskmanagementsystem'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'taskmanagementsystem'
 
 
-    // host: '185.38.44.227',
-    // user: 'citykaro_ahmed',
-    // password: 'attari11',
-    // database: 'citykaro_task_manage'
+    host: '185.38.44.227',
+    user: 'citykaro_ahmed',
+    password: 'attari11',
+    database: 'citykaro_task_manage'
 });
