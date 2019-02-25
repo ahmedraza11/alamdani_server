d12680b5bc55804818e1cf15ac9f9a741e0e0d4c
Ahmed Raza Qadri <razatechuc11@gmail.com>
2019-02-25 20:59:37 +0500

    server is ready to go

--- server.js
+++ server.js
@@ -3,13 +3,29 @@ const app = require('./app');
 const port = process.env.PORT || 3001;
 const connection = require('./api/config/db');
 
-connection.connect((err) => {
-    if (err) {
-        console.error("Error in Connection", err);
-    } else {
-        console.log("Connected as id", connection.threadId);
-    }
-})
+
+function handleDisconnect() {
+   // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
 
 const server = http.createServer(app);
 
