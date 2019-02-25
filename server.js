fd9f1b33245b09c42ac209677957a9d85eeec83e
Ahmed Raza Qadri <razatechuc11@gmail.com>
2019-02-25 20:17:03 +0500

    server is ready to go

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3001;
+const connection = require('./api/config/db');
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Error in Connection", err);
+    } else {
+        console.log("Connected as id", connection.threadId);
+    }
+})
 
 const server = http.createServer(app);
 
