d49e061d5951bbb33d422d6863062df2f158b3cb
Ahmed Raza Qadri <razatechuc11@gmail.com>
2019-02-27 13:07:47 +0500

    mysql config managed

--- api/config/db.js
+++ api/config/db.js
@@ -8,9 +8,15 @@ module.exports = mysql.createConnection({
     // database: 'taskmanagementsystem'
 
 
-    host: '185.38.44.227',
-    user: 'citykaro_ahmed',
-    password: 'attari11',
-    database: 'citykaro_task_manage'
+    // host: '185.38.44.227',
+    // user: 'citykaro_ahmed',
+    // password: 'attari11',
+    // database: 'citykaro_task_manage'
+
+
+    host: '173.82.245.150',
+    user: 'almadani_admin',
+    password: 'almadaniadmin',
+    database: 'almadani_task_manage'
     // port:3306,
 });
